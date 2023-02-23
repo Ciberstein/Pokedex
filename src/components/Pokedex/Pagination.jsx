22a9f7fd343e3858234b8068dfc5cd5529629b71
cerberus8888 <v26396341@gmail.com>
2023-02-23 00:53:36 -0500

    pagination

--- src/components/Pokedex/Pagination.jsx
+++ src/components/Pokedex/Pagination.jsx
@@ -25,7 +25,7 @@ export const Pagination = ({ pokePerPage, totalPokes, paginate, currentPage, set
                 <li>
                     <select className='page-link-select' onChange={handlePaginate}>
                         <option value='4'>4</option>
-                        <option value='8'>8</option>
+                        <option value='8' selected>8</option>
                         <option value='12'>12</option>
                         <option value='16'>16</option>
                         <option value='20'>20</option>
